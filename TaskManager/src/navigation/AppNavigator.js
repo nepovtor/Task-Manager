df230927be4157743cc44d3e4f4563929a275a2c
nepovtor <111302019+nepovtor@users.noreply.github.com>
2025-07-30 14:57:53 +0300

    Create AppNavigator.js

---
+++ TaskManager/src/navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TaskListScreen from '../screens/TaskListScreen';
+import TaskFormScreen from '../screens/TaskFormScreen';
+import TaskDetailScreen from '../screens/TaskDetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TaskList">
+        <Stack.Screen name="TaskList" component={TaskListScreen} options={{ title: 'Список задач' }} />
+        <Stack.Screen name="TaskForm" component={TaskFormScreen} options={{ title: 'Новая задача' }} />
+        <Stack.Screen name="TaskDetail" component={TaskDetailScreen} options={{ title: 'Детали задачи' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
