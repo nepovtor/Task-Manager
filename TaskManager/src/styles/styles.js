f3a1460a3c3aa3e2ffa6c8b124e53491dcec5138
nepovtor <111302019+nepovtor@users.noreply.github.com>
2025-07-30 14:58:15 +0300

    Create styles.js

---
+++ TaskManager/src/styles/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#F9FAFB',
+  },
+  card: {
+    marginVertical: 8,
+    padding: 8,
+    backgroundColor: '#FFFFFF',
+  },
+  input: {
+    marginBottom: 12,
+  },
+  addButton: {
+    marginTop: 16,
+    backgroundColor: '#2563EB',
+  },
+  saveButton: {
+    marginTop: 16,
+    backgroundColor: '#2563EB',
+  },
+  deleteButton: {
+    marginTop: 16,
+    backgroundColor: '#DC2626',
+  },
+  detailTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 12,
+  },
+});
