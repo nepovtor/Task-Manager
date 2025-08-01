cbbfa9c95098833dc57dcc02619cef780b2a23b6
nepovtor <111302019+nepovtor@users.noreply.github.com>
2025-08-01 22:28:26 +0300

    Update index.js

--- TaskManager/src/i18n/index.js
+++ TaskManager/src/i18n/index.js
@@ -1,6 +1,7 @@
 import * as Localization from 'expo-localization';
 import { I18n } from 'i18n-js';
 
+// Объявляем переводы
 const i18n = new I18n({
   en: {
     taskList: 'Task List',
@@ -22,7 +23,11 @@ const i18n = new I18n({
   },
 });
 
+// Разрешить fallback при отсутствии перевода
 i18n.fallbacks = true;
-i18n.locale = Localization.locale;
+
+// Безопасная установка языка
+const locale = typeof Localization.locale === 'string' ? Localization.locale.split('-')[0] : 'en';
+i18n.locale = locale;
 
 export default i18n;
